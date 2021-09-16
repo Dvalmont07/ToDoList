ee6c4ce54638d9da71fe163b84e9b22109996272
Danilo Matos <dvalmont07@hotmail.com>
2021-09-16 01:53:55 -0300

    first commit - add and remove methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoList';
+  taskName: string = "";
+
+  taskList: any[] = [];
+
+  addTask() {
+    let order = this.taskList.length > 0 ? Math.max(...this.taskList.map(x => { return x.Order })) + 1 : 1;
+
+    this.taskList.push({ TaskName: this.taskName, Order: order });
+    this.taskName = "";
+    if (this.taskList.length > 0) {
+      console.log("AAA", this.taskList);
+      console.log("PPP", this.taskList.map(x => { return x.Order }));
+
+    }
+  }
+  removeTask(order: any) {
+
+    this.taskList = this.taskList.filter(task => {
+      return task.Order != order;
+    });
+  }
+
 }
