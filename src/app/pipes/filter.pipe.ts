089f40c3a7eef7996dd3e1c770b4f8aa0dc60d82
Danilo Matos <dvalmont07@hotmail.com>
2021-09-22 10:01:27 -0300

    applying pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,12 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(items: any[], searchText: string): any[] {
+
+    console.log("items", items);
+
+    if (!items) { return []; }
+    if (searchText == "") { return items; }
+
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(item => {
+      return item.TaskName.toLocaleLowerCase().includes(searchText);
+    });
   }
 
 }
