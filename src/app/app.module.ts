b93b6308a7f07a4067cc2b980ff2b871473eb6ee
Danilo Matos <dvalmont07@hotmail.com>
2021-09-22 01:14:39 -0300

    add pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MultipleSearchFilterPipe } from './pipes/multiple-search-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultipleSearchFilterPipe
   ],
   imports: [
     BrowserModule,
