7fc6c11749dcbbe04b9d4c1ad406fa3d554e96ef
Danilo Matos <danilo.dejesusmatos@gmail.com>
2021-10-21 01:52:31 -0300

    update

--- src/app/interfaces/myTask.ts
+++ src/app/interfaces/myTask.ts
@@ -1,16 +1,14 @@
 export interface MyTask {
-    Id?: number;
-    TaskName: string;
-    Order: number;
-    Done: boolean;
-    Today: boolean;
-    //TODO  ScheduledTo?: boolean
-    //TODO RepeatyType Enum [daily, weekdeays, weekly, monthly, annualy, custom ]
-    //if custum
-    //TODO RepeatQuntity:
-    //TODO RepeatUnit: Number
+  Id?: number;
+  TaskName: string;
+  Order: number;
+  Done: boolean;
+  Today: boolean;
+  //TODO  ScheduledTo?: boolean
+  //TODO RepeatyType Enum [daily, weekdeays, weekly, monthly, annualy, custom ]
+  //if custum
+  //TODO RepeatQuntity:
+  //TODO RepeatUnit: Number
 
-    //TODO remindMe: Date
-
+  //TODO remindMe: Date
 }
-
--- src/app/services/mocks/TasksList.ts
+++ src/app/services/mocks/TasksList.ts
@@ -1,12 +1,12 @@
-import { MyTask } from "src/app/interfaces/myTask";
+import { MyTask } from 'src/app/interfaces/myTask';
 
 export const TODOTASKSLIST: MyTask[] = [
-    { TaskName: "Task 01", Order: 1 },
-    { TaskName: "Task 02", Order: 2 }
-]
+  { TaskName: 'Task 01', Order: 1, Done: false, Today: false },
+  { TaskName: 'Task 02', Order: 2, Done: false, Today: false },
+];
 
 export const DONETASKSLIST: MyTask[] = [
-    { TaskName: "Done Task 01", Order: 1 },
-    { TaskName: "Done Task 02", Order: 2 },
-    { TaskName: "Done Task 03", Order: 3 }
-]
+  { TaskName: 'Done Task 01', Order: 1, Done: true, Today: false },
+  { TaskName: 'Done Task 02', Order: 2, Done: true, Today: false },
+  { TaskName: 'Done Task 03', Order: 3, Done: true, Today: false },
+];
